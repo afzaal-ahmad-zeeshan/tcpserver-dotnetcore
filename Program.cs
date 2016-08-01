c8ba8c59271743e6e091f28e6c68cbf08e5ed8f7
Afzaal Ahmad Zeeshan <justin17862@gmail.com>
2016-08-02 00:11:34 +0500

    Publishing content to the repository.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TCPServer.App.AfzaalAhmadZeeshan
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the port where to run the TCP server: ");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            TcpServer.StartServer(port);
+            TcpServer.Listen();
+        }
+    }
+}
